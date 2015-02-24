67a362cdc3976f9a156d0c4bfdd0093858f8cc2e
mcgr0217 <Samantha@samanthas-MacBook-Pro.local>
2015-02-24 12:49:11 -0500

    Adding classes and css page
    Adding classes in the index page and adding the css page for styling
    afterwards.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-var $form = $('form');
+var $form = $('.form');
+var $add = $('#todo');
+var $new = $('.list');
+
+$form.on('submit', function (e) {
+    var $li = $('<li>');
+    e.preventDefault();
+    $li.append($todo.val());
+    $list.append($li);
+    $li.on('click', function () {
+    $li.remove();
+    });
+});
